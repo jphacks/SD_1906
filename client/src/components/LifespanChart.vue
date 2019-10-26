52fbe81f915bbaa7baa94e56ea2bb43c9a119519
shion-uni <hficndo71h80@gmail.com>
2019-10-26 19:19:49 +0900

    LifeSpanChartプロトタイプ完成

--- client/src/components/LifespanChart.vue
+++ client/src/components/LifespanChart.vue
@@ -2,9 +2,10 @@
     import { Bar } from 'vue-chartjs';
 
     var hitoketa_data = [];
-    for (var i = 100; i >= 0; i--){
-        x_axis.push(100 - i);
-        hitoketa_data.push(Math.floor(Math.random() * 11));
+    var x_axis = [];
+    for (var i = 120; i >= 0; i--){
+        x_axis.push(i);
+        hitoketa_data.push(i);
     }
 
     export default {
@@ -13,15 +14,21 @@
         data () {
             return {
                 data: {
-                    labels: hitoketa_data,
+                    labels: x_axis,
                     datasets: [
                         {
+                            fillColor: 'rgba(0, 0, 255, 0.5)',
+                            label: '獲得寿命',
                             data: hitoketa_data,
-                            backgroundColor: hitoketa_data.map(function(num){
+                            backgroundColor: x_axis.map(function(num){
                                 var r = 2.55 * (100 - num);
                                 var g = 2.55 * num;
                                 return 'rgba(' + r.toString() + ', ' + g.toString() + ', 0)';
                             }),
+                            backgroundColor: 'rgba(94, 255, 45, 0.6)',
+                            fill: true,
+                            type: 'line',
+                            lineTension: 0.1,
 
                         }
                     ]
@@ -31,11 +38,12 @@
                         xAxes: [{
                             scaleLabel: {
                                 display: true,
+                                labelString: '分前',
                             }
                         }],
                         yAxes: [{
                             ticks: {
-                                beginAtZero: true,
+                                // beginAtZero: Ture,
                                 stepSize: 10,
                             }
                         }]
