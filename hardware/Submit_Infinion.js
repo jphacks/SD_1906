9a5468c86c27fbd1d1346b4ef538d1984688b806
AoiSuzuki <katakatakatan0117@gmail.com>
2020-03-14 18:29:22 +0900

    update_submitinfinion

--- hardware/Submit_Infinion.js
+++ hardware/Submit_Infinion.js
@@ -1,9 +1,11 @@
-// JavaScript source code
+﻿// JavaScript source code
 const Obniz = require("obniz");
 var DPS310 = require("./DPS310");
 
-var obniz1 = new Obniz("1552-7989");
-var obniz2 = new Obniz("9800-4747");
+var obniz1 = new Obniz("9735-6357");
+var obniz2 = new Obniz("6379-9359");
+
+var posture = 0;
 
 
 var getPast = (function () {
@@ -16,138 +18,72 @@ var getPast = (function () {
     }
 })();
 
-var onLED5 = false;
-var onLED6 = false;
-var onLED7 = false;
-var onLED8 = false;
-var onLED9 = false;
-var onLED10 = false;
-
-function adjustLED() {
-    obniz2.io5.output(onLED5);
-    obniz2.io6.output(onLED6);
-    obniz2.io7.output(onLED7);
-    obniz2.io8.output(onLED8);
-    obniz2.io9.output(onLED9);
-    obniz2.io10.output(onLED10);
-}
-
+//姿勢推定・DPS３１０
 obniz1.onconnect = async function () {
 
     Obniz.PartsRegistrate(DPS310);
     obniz1.setVccGnd(4, 3, '5v');
     var sensor = obniz1.wired("DPS310", { sda: 0, scl: 1, gnd: 2 });
     let pastData = NaN;
-    const check_pressure_dif = 100;
-    var angle = 30;
-    var posture = 0;
+    const check_pressure_dif = 10;
     await sensor.init();
-    moveMotor(80, 0);
-    console.log("set angle");
 
-    postIsSitting(isSitting);
+    obniz1.display.clear();
+    obniz1.display.print("Posture Sensing");
 
-    while (1) {
+    obniz1.repeat(async function () {
 
         let nowData = await sensor.measurePressureOnce();
         var pressure_dif = nowData - pastData;
-        var elapsedTime = 0;
-        var PastTime1 = 0;
-        var PastTime2 = 0;
         console.log(pressure_dif);
 
-        if (pressure_dif > check_pressure_dif) {
-
-            var sittingTime = Date.now();
-            angle = 30;
+        if (pressure_dif < check_pressure_dif) {
+            posture = 1;
+            console.log("Standing");
+        } else if (pressure_dif > check_pressure_dif) {
             posture = 0;
-            console.log("sitting");
-            isSitting = true;
-            postIsSitting(isSitting);
+            console.log("Sitting");
 
             while (pressure_dif > -check_pressure_dif) {
-                nowData = await sensor.measurePressureOnce();
-                pressure_dif = nowData - pastData;
+                let nowData = await sensor.measurePressureOnce();
+                var pressure_dif = nowData - pastData;
                 console.log(pressure_dif);
-                console.log("keep sitting");
+
                 pastData = nowData;
-                PastTime1 = getPast();
-                elapsedTime = PastTime1 + PastTime2;
-                PastTime2 = elapsedTime;
-                obniz1.wait(300);
 
-                if (elapsedTime > 1800000) {
-                    moveMotor(angle, posture);
-                    angle--;
-                }
+                await obniz1.wait(1000);
             }
-
-        } else if (pressure_dif < -check_pressure_dif + 70) {
-
-            var standingTime = Date.now();
-            angle = 0;
-            posture = 1;
-            console.log("standing");
-            isSitting = false;
-            postIsSitting(isSitting);
-
-            while (pressure_dif < check_pressure_dif) {
-                nowData = await sensor.measurePressureOnce();
-                pressure_dif = nowData - pastData;
-                console.log(pressure_dif);
-                console.log("keep standing");
-                pastData = nowData;
-                PastTime1 = getPast();
-                elapsedTime = PastTime1 + PastTime2;
-                PastTime2 = elapsedTime;
-                obniz1.wait(300);
-
-                if (elapsedTime > 60000) {
-                    moveMotor(angle, posture);
-                    angle++;
-                }
-            }
-
+            console.log("Standing");
         }
+        
         pastData = nowData;
-        obniz1.wait(1000);
-    }
+    }, 1000);
 }
-moveMotor = async function (angle, posture) {
-    var obniz2 = new Obniz("9800-4747");
-    obniz2.onconnect = async function () {
 
-        var leaf1 = obniz2.wired("ServoMotor", { signal: 1, gnd: 0 });
-        var leaf2 = obniz2.wired("ServoMotor", { signal: 2 });
-        var leaf3 = obniz2.wired("ServoMotor", { signal: 3 });
-        obniz2.io4.output(false);
+obniz2.onconnect = async function () {
 
-        if (angle > 0 && posture == 0) {
-            leaf1.angle(angle);
-            leaf2.angle(180 - angle);
-            leaf3.angle(angle);
-            console.log("move leaf");
-            if (angle < 29 && onLED5) onLED5 = false;
-            else if (angle < 25 && onLED6) onLED6 = false;
-            else if (angle < 20 && onLED7) onLED7 = false;
-            else if (angle < 15 && onLED8) onLED8 = false;
-            else if (angle < 10 && onLED9) onLED9 = false;
-            else if (angle < 5 && onLED10) onLED10 = false;
-            adjustLED();
+    obniz2.setVccGnd(4, 3, '5v');
+    var leaf1 = obniz2.wired("ServoMotor", { signal: 1, gnd: 0 });
+    var leaf2 = obniz2.wired("ServoMotor", { signal: 2 });
+    var leaf3 = obniz2.wired("ServoMotor", { signal: 3 });
+    var i = 0;
+
+    obniz2.display.clear();
+    obniz2.display.print("Move ServoMotor");
+
+    obniz2.repeat(async function () {
+
+        if (posture == 1) {
+            leaf1.angle(0);
+            i = 0;
+        } else if (posture == 0 && i < 100) {
+            leaf1.angle(i);
+            i += 5;
+            await obniz2.wait(300);
+        } else if (posture == 0 && i >= 100){
+            console.log("Stand Up!!")
         }
 
-        if (angle < 30 && posture == 1) {
-            leaf1.angle(angle);
-            leaf2.angle(180 - angle);
-            leaf3.angle(angle);
-            if (angle < 29 && !onLED5) onLED5 = true;
-            else if (angle < 25 && !onLED6) onLED6 = true;
-            else if (angle < 20 && !onLED7) onLED7 = true;
-            else if (angle < 15 && !onLED8) onLED8 = true;
-            else if (angle < 10 && !onLED9) onLED9 = true;
-            else if (angle < 5 && !onLED10) onLED10 = true;
-            adjustLED();
-        }
+    }, 1000);
 
-    }
 }
